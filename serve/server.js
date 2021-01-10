7886853d1088069d221f1e46474cf3aae2114414
juanjo <juanjobenavides314@gmail.com>
2021-01-10 15:38:09 -0500

    add controllers y models

--- serve/server.js
+++ serve/server.js
@@ -1,34 +1,14 @@
 const express=require('express');
-require('./config/config')
 const app=express();
+require('dotenv').config();
+const {conectarDB}=require('../db/config')
+conectarDB();
 const bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 app.use(bodyParser.json())
 
-app.get('/usuarios',(req,res)=>{
-    res.json('GET')
-})
-app.post('/usuarios',(req,res)=>{
-    const body=req.body;
-    if(body.nombre===undefined){
-        return res.status(400).json({
-            type:'error',
-            message:'El nombre es obligatorio'
-        })
-    }
-    res.json({
-        body
-    })
-})
-app.put('/usuarios/:id',(req,res)=>{
-    const id=req.params.id;
-    res.json({id})
-})
-app.delete('/usuarios/:id',(req,res)=>{
-    const id=req.params.id;
-    res.json({id})
-})
+app.use(require('./routes/usuarios'))
 
-app.listen(process.env.PORT,()=>console.log('Run...'))
+app.listen(process.env.port,()=>console.log('Run...'))
